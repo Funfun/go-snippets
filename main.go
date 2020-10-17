dd055c873ad7f755916ae1cbaf65ee11557cd904
Tsyren Ochirov <tsyren@messagebird.com>
2020-10-17 16:24:13 +0200

    empty configurable server

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"html"
 	"log"
 	"net/http"
 )
 
+var (
+	addr = flag.String("addr", "0.0.0.0:8080", "host:port of listen http server")
+)
+
 func main() {
-	log.Fatal(http.ListenAndServe(":13001", nil))
+	flag.Parse()
+
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
