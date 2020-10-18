2b3ab7d245287695018111f6f3d12343b12bfb7e
Tsyren Ochirov <tsyren@messagebird.com>
2020-10-18 20:36:17 +0200

    clean ups and add more endpoints

--- kafka-go-avro/consumer/main.go
+++ kafka-go-avro/consumer/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/segmentio/kafka-go"
 )
 
 var (
@@ -12,19 +14,30 @@ var (
 )
 
 func messagesHandler(c *gin.Context) {
-	consumer, err := NewConsumer(*kafkaAddr, *kafkaTopic)
-	if err != nil {
-		c.JSON(500, gin.H{
-			"status": "failed",
-			"error":  err.Error(),
-		})
+	// to consume messages
+	topic := c.Query("topic")
+	// make a new reader that consumes from topic-A, partition 0, at offset 42
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:   []string{*kafkaAddr},
+		Topic:     topic,
+		Partition: 0,
+		MinBytes:  10e3, // 10KB
+		MaxBytes:  10e6, // 10MB
+	})
+	r.SetOffset(42)
 
-		return
+	var ms []string
+
+	for {
+		m, err := r.ReadMessage(c.Request.Context())
+		if err != nil {
+			break
+		}
+		log.Printf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+		ms = append(ms, string(m.Value))
 	}
-	defer consumer.Close()
 
-	m, err := consumer.Read()
-	if err != nil {
+	if err := r.Close(); err != nil {
 		c.JSON(500, gin.H{
 			"status": "failed",
 			"error":  err.Error(),
@@ -34,7 +47,7 @@ func messagesHandler(c *gin.Context) {
 	}
 
 	c.JSON(200, gin.H{
-		"messages": m,
+		"messages": ms,
 	})
 }
 
--- kafka-go-avro/producer/main.go
+++ kafka-go-avro/producer/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"context"
 	"flag"
 
 	"github.com/gin-gonic/gin"
+	"github.com/segmentio/kafka-go"
 )
 
 var (
-	kafkaTopic = flag.String("topic", "default", "Kafka topic")
+	kafkaTopic = flag.String("topic", "my-topic", "Kafka topic")
 	kafkaAddr  = flag.String("addr", "localhost:9092", "Kafka connection address")
 )
 
 func newMessageHandler(c *gin.Context) {
-	producer, err := NewProducer(*kafkaAddr, *kafkaTopic)
+	topic := c.PostForm("topic")
+	producer, err := NewProducer(*kafkaAddr, topic)
 	if err != nil {
 		c.JSON(500, gin.H{
 			"status": "failed",
@@ -43,10 +46,85 @@ func newMessageHandler(c *gin.Context) {
 	})
 }
 
+func createTopic(c *gin.Context) {
+	// to create topics
+	topic := c.PostForm("topic")
+	partition := 0
+
+	conn, err := kafka.DialLeader(context.Background(), "tcp", *kafkaAddr, topic, partition)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"status": "failed",
+			"error":  err.Error(),
+		})
+	}
+	defer conn.Close()
+
+	topicConfigs := []kafka.TopicConfig{
+		{
+			Topic:             topic,
+			NumPartitions:     1,
+			ReplicationFactor: 1,
+		},
+	}
+
+	err = conn.CreateTopics(topicConfigs...)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"status": "failed",
+			"error":  err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"status": "created",
+		"info": gin.H{
+			"topic": topic,
+			"addr":  *kafkaAddr,
+		},
+	})
+}
+
+func listTopics(c *gin.Context) {
+	conn, err := kafka.Dial("tcp", *kafkaAddr)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"status": "failed",
+			"error":  err.Error(),
+		})
+	}
+	defer conn.Close()
+
+	partitions, err := conn.ReadPartitions()
+	if err != nil {
+		c.JSON(500, gin.H{
+			"status": "failed",
+			"error":  err.Error(),
+		})
+	}
+
+	m := map[string]struct{}{}
+
+	for _, p := range partitions {
+		m[p.Topic] = struct{}{}
+	}
+
+	c.JSON(200, gin.H{
+		"info": gin.H{
+			"topics": m,
+			"addr":   *kafkaAddr,
+		},
+	})
+}
+
 func main() {
 	flag.Parse()
 
 	r := gin.Default()
 	r.POST("/messages", newMessageHandler)
-	r.Run()
+	r.POST("/topic", createTopic)
+	r.GET("/topics", listTopics)
+	r.Run(":8082")
 }
