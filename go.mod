e93bfda23608d0c5c614dc39635e6919ce052b85
Tsyren Ochirov <tsyren@messagebird.com>
2020-10-25 15:44:24 +0100

    wrap test server into github.com/urfave/cli

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Funfun/testserver
+
+go 1.15
+
+require github.com/urfave/cli v1.22.4
--- main.go
+++ main.go
@@ -1,22 +1,60 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"html"
 	"log"
 	"net/http"
-)
+	"os"
 
-var (
-	addr = flag.String("addr", "0.0.0.0:8080", "host:port of listen http server")
+	"github.com/urfave/cli"
 )
 
 func main() {
-	flag.Parse()
-
-	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
-	log.Fatal(http.ListenAndServe(*addr, nil))
+
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "addr",
+				Value: "0.0.0.0:8080",
+				Usage: "host:port of listen http server",
+			},
+		},
+		Name:  "YourSuperApp",
+		Usage: "You are about to start YourSuperApp",
+		Commands: []*cli.Command{
+			{
+				Name:    "server",
+				Aliases: []string{"s"},
+				Usage:   "your server api",
+				Action: func(c *cli.Context) error {
+					addr := c.String("addr")
+					fmt.Printf("Starting the Test Server at %s\n", addr)
+
+					return http.ListenAndServe(addr, nil)
+				},
+			},
+			{
+				Name:    "resource",
+				Aliases: []string{"get"},
+				Usage:   "your rest api calls",
+				Action: func(c *cli.Context) error {
+					resource := c.Args().First()
+
+					fmt.Println("Warning: command is no-op")
+					fmt.Printf("Requesting resource: %s\n", resource)
+
+					return nil
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
