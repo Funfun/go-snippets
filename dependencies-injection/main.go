4c8f3151da253c2223b1fb97246011050aa7a3e6
Tsyren <nsu1team@gmail.com>
2021-10-16 01:11:02 +0800

    add one more note about dep injection

--- dependencies-injection/main.go
+++ dependencies-injection/main.go
@@ -26,6 +26,17 @@ func main() {
 
 	// 2. Using Variadic Functions with Option Functions
 	_ = NewClient(WithURL("https://secure.example"))
+
+	// 3. pass interfaces, return structs
+	storage := Storage{}
+	_ = NewCustomer(storage)
+}
+
+type Storage struct {
+}
+
+func (s Storage) Data() []string {
+	return []string{"Dummy"}
 }
 
 type ClientConfig struct {
@@ -52,4 +63,15 @@ func WithURL(url string) func(client *Client) {
 	return func(client *Client) {
 		client.url = url
 	}
+}
+
+type Share interface {
+	Data() []string
+}
+type Account struct {
+	Info []string
+}
+
+func NewCustomer(share Share) *Account {
+	return &Account{Info: share.Data()}
 }
